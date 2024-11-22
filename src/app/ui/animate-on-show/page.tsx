b50f97255d0d6ab8d4ec03cdf5b9f48c63e28e8a
Gabriel Ramalho <biel-pedro@hotmail.com>
2024-11-22 14:32:21 -0300

    feat: add new component

--- src/app/ui/animate-on-show/page.tsx
+++ src/app/ui/animate-on-show/page.tsx
@@ -1,12 +1,30 @@
+'use client'
+
+import { motion } from 'framer-motion'
+
 export default function AnimateOnShow() {
   return (
-    <div className="flex flex-1 justify-center items-center py-40">
-      <button
-        type="button"
-        className="bg-neutral-100 text-neutral-950 text-sm font-medium px-4 py-1.5 rounded-lg hover:bg-neutral-200"
-      >
-        Press me
-      </button>
+    <div className="flex flex-1 justify-center items-center py-20">
+      <div className="flex gap-4">
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.3, delay: 0.1, ease: 'linear' }}
+          className="size-40 bg-red-500 rounded-lg"
+        />
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.3, delay: 0.3, ease: 'linear' }}
+          className="size-40 bg-red-500 rounded-lg"
+        />
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.3, delay: 0.5, ease: 'linear' }}
+          className="size-40 bg-red-500 rounded-lg"
+        />
+      </div>
     </div>
   )
 }
