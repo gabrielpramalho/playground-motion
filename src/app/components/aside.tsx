21ac655c63de0e6b1af9f37c79c114f74b30bc79
Gabriel Ramalho <biel-pedro@hotmail.com>
2024-11-25 21:20:05 -0300

    feat: create array to aside items

--- src/app/components/aside.tsx
+++ src/app/components/aside.tsx
@@ -1,17 +1,38 @@
 import { NavLink } from './nav-link'
 
 export function Aside() {
+  const items = [
+    {
+      title: 'Animation',
+      href: '/ui/animate-on-show',
+    },
+    {
+      title: 'Button',
+      href: '/ui/button',
+    },
+    {
+      title: 'Magnetic Hover',
+      href: '/ui/magnetic-hover',
+    },
+  ]
+
+  const asideItemsSorted = items.sort((a, b) => a.title.localeCompare(b.title))
+
   return (
     <div className="flex-shrink-0 lg:w-[270px]">
       <aside className="bottom-0 top-14 h-[calc(100vh_-_3rem)] flex-1 border-r border border-neutral-800/60 max-lg:hidden max-lg:pt-12 lg:sticky">
         <nav className="h-full overflow-y-auto pl-6 pr-6 pt-8 space-y-4 [-ms-overflow-style:none] [scrollbar-width:none] max-lg:hidden [&::-webkit-scrollbar]:hidden">
           <div>
-            <span className='text-lg'>Components</span>
+            <span className="text-lg">Components</span>
           </div>
           <ul className="flex flex-col gap-4">
-            <NavLink href="/ui/animate-on-show">Animation</NavLink>
-            <NavLink href="/ui/button">Button</NavLink>
-            <NavLink href="/ui/magnetic-hover">Magnetic Hover</NavLink>
+            {asideItemsSorted.map(item => {
+              return (
+                <NavLink key={item.href} href={item.href}>
+                  {item.title}
+                </NavLink>
+              )
+            })}
           </ul>
         </nav>
       </aside>
