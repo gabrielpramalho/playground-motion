1539ba31716339fee575295b39f97273f856f825
Gabriel Ramalho <biel-pedro@hotmail.com>
2025-01-21 22:11:00 -0300

    feat: add some transition

--- src/app/ui/input-file/_components/input-file.tsx
+++ src/app/ui/input-file/_components/input-file.tsx
@@ -7,13 +7,13 @@ export function InputFile() {
         htmlFor="file"
         className="flex items-center justify-center p-6 border rounded-lg cursor-pointer group"
       >
-        <div className='p-8'>
+        <div className="p-8 space-y-4">
           <div className="flex justify-center gap-2">
-            <div className="group-hover:-translate-y-2">
-              <CloudUpload />
+            <div className="group-hover:-translate-y-2 transition-transform duration-600 [transition-timing-function:cubic-bezier(0.25,0.1,0.25,1)]">
+              <CloudUpload className='size-6' />
             </div>
           </div>
-          <span>Click or drop your files here</span>
+          <span className='block'>Click or drop your files here</span>
         </div>
       </label>
 
